65d46c6b20f41de826a0154bbfa01e7c344d827f
Alaa Ekky <ekkya@tcd.ie>
2017-12-11 12:14:24 +0000

    Graph plotted

--- manager.py
+++ manager.py
@@ -3,7 +3,8 @@ import time
 import sqlite3
 import git
 import sys
-
+import numpy as np
+import matplotlib.pyplot as plt
 from rq import Connection, Queue
 
 from CC import get_complexity
@@ -21,70 +22,80 @@ def main():
     repo = git.Repo(repo_dir)
     # Kick off the tasks asynchronously
     async_results = {}
-    connection = sqlite3.connect('results_4w.db')
-    #print "Database Opened"
-    #connection.execute('''CREATE TABLE RESULTS
-     #                                        (FileName TEXT PRIMARY KEY     NOT NULL,
-      #                                        CC            REAL,
-       #                                      TimeTaken      REAL    NOT NULL);''')
-    #connection.execute('''CREATE TABLE WORKERS
-     #                                            (WORKER INT PRIMARY KEY     NOT NULL,
-      #                                           TimeTaken      REAL    NOT NULL);''')
-    #print "Table created successfully"
+    connection = sqlite3.connect('results.db')
+    print "Database Opened"
+    connection.execute('''CREATE TABLE RESULTS
+                                             (FileName TEXT PRIMARY KEY     NOT NULL,
+                                              CC            REAL,
+                                             TimeTaken      REAL    NOT NULL);''')
+    connection.execute('''CREATE TABLE WORKERS
+                                                 (WORKER INT PRIMARY KEY     NOT NULL,
+                                                 TimeTaken      REAL    NOT NULL);''')
+    print "Table created successfully"
     q = Queue()
-    #commits_touching_path = list(repo.iter_commits('master'))
-    #for root, dirs, files in os.walk(repo_dir):
-     #   for file in files:
-      #      for i in range(len(commits_touching_path)):
-       #         commits = commits_touching_path[i]
-        #        if file.endswith(".py"):
-         #           try:
-          #              file_contents = repo.git.show('{}:{}'.format(commits.hexsha, file))
-           #             # print file_contents
-            #            f1 = open(directory + 'test_' + str(i) + '.py', 'a')
-             #           f1.write(file_contents)
-              #      except git.exc.GitCommandError:
-               #         continue
-    #f1.close()
+    commits_touching_path = list(repo.iter_commits('master'))
+    for root, dirs, files in os.walk(repo_dir):
+        for file in files:
+            for i in range(len(commits_touching_path)):
+                commits = commits_touching_path[i]
+                if file.endswith(".py"):
+                    try:
+                        file_contents = repo.git.show('{}:{}'.format(commits.hexsha, file))
+                        # print file_contents
+                        f1 = open(directory + 'test_' + str(i) + '.py', 'a')
+                        f1.write(file_contents)
+                    except git.exc.GitCommandError:
+                        continue
+    f1.close()
     for root, dirs, files in os.walk(directory):
         for file in files:
             q1 = str(os.path.join(directory, file))
             async_results[file] = q.enqueue(get_complexity, q1)  ##CHANGE TO PATH OF F1
-            #start_time = time.time()
 
-    worker = 30
+    worker = 50
     start_time = time.time()
     done = False
     while not done:
         os.system('clear')
         print('Asynchronously: (now = %.2f)' % (time.time() - start_time,))
         done = True
-        for root, dirs, files in os.walk(directory):
-            for file in files:
-                if file.endswith(".py"):
-                    result = async_results[file].return_value
-                    #print result
-                    if result is None:
-                        done = False
-                        result = '(calculating)'
-                        #print('CC(%s) = %s' % (filename, result))
-                    #total_time = time.time() - start_time
-                    if result != '(calculating)':
-                        end_time = time.time()
-                        total_time = end_time - start_time
-                        cursor = connection.execute("INSERT OR REPLACE INTO RESULTS VALUES (?, ?, ?)",
+        result = async_results[file].return_value
+        if result is None:
+            done = False
+            result = '(calculating)'
+        if result != '(calculating)':
+            end_time = time.time()
+            total_time = end_time - start_time
+            cursor = connection.execute("INSERT OR REPLACE INTO RESULTS VALUES (?, ?, ?)",
                                                     (file, result, total_time))
-                        connection.commit()
-                    else:
-                        print "Continue"
-    #total_time = end_time - start_time
+            connection.commit()
+        else:
+            print "Continue"
     print total_time
     cursor = connection.execute("INSERT OR REPLACE INTO WORKERS VALUES (?, ?)",
                                 (worker, total_time))
     connection.commit()
 
     print('Done')
+    x = []
+    y = []
+    cursor1 = connection.execute("SELECT WORKER, TimeTaken FROM WORKERS")
+    for row in cursor1.fetchall():
+        x.append(row[0])
+        y.append(row[1])
+        #print x
+        #print y
 
+    print x
+    print y
+
+    #y_pos = np.arange(len(x))
+    plt.plot(x, y, 'r')
+    plt.xlabel('Number of Workers')
+    plt.ylabel('Time')
+    plt.title('Performance of Workers')
+    plt.bar(x, y)
+    plt.show()
 if __name__ == '__main__':
     # Tell RQ what Redis connection to use
     with Connection():
