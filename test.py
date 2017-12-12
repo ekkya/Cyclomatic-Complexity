a5aa270c10fcfe005ac06ec3fa12df642912fc17
Alaa Ekky <ekkya@tcd.ie>
2017-12-12 21:22:36 +0000

    Results represented as a .png file

--- test.py
+++ test.py
@@ -1,30 +1,26 @@
 import os.path
 import git
 import sys
+import matplotlib.pyplot as plt
+import sqlite3
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
+x = []
+y = []
+connection = sqlite3.connect('results.db')
+cursor1 = connection.execute("SELECT WORKER, TimeTaken FROM WORKERS")
+for row in cursor1.fetchall():
+    x.append(row[0])
+    y.append(row[1])
+        #print x
+        #print y
 
-repo = git.Repo()
-git_url = "https://github.com/geekcomputers/Python.git"
-repo_dir = "/home/ekkya/Cyclomatic-Complexity/test"
-repo.clone_from(git_url, repo_dir)
-print "1"
-repo = git.Repo(repo_dir)
-commits_touching_path = list(repo.iter_commits('master'))
-print commits_touching_path
-print len(commits_touching_path)
-print range(len(commits_touching_path))
-for root, dirs, files in os.walk(repo_dir):
-    for file in files:
-        for i in range(len(commits_touching_path)):
-            commits = commits_touching_path[i]
-            if file.endswith(".py"):
-                try:
-                    file_contents = repo.git.show('{}:{}'.format(commits.hexsha, file))
-                    #print file_contents
-                    f1 = open('test_' + str(i) + '.py', 'a')
-                    f1.write(file_contents)
-                except git.exc.GitCommandError:
-                    continue
-f1.close()
+print x
+print y
+
+    #y_pos = np.arange(len(x))
+plt.plot(x, y, 'r')
+plt.xlabel('Number of Workers')
+plt.ylabel('Time')
+plt.title('Performance of Workers')
+plt.bar(x, y)
+plt.show()
